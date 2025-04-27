677d5b4d42c5b8318d21296ef4741e341f83a28d
Stephan Chiorean <stephanchiorean@Mac.hsd1.wa.comcast.net>
2025-04-27 00:43:29 -0700

    finalize ts migration

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,5 +1,6 @@
-/** @type {import('tailwindcss').Config} */
-export default {
+import type { Config } from 'tailwindcss';
+
+const config: Config = {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
@@ -18,4 +19,6 @@ export default {
     },
   },
   plugins: [],
-} 
+};
+
+export default config; 
